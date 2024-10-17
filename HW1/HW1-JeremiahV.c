d0682fcf2ae1baca413f808caca0c2d2a681cab9
jvandag <j.c.vandagrift@gmail.com>
2024-10-16 18:41:18 -0700

    10-16-24 notes

--- HW1/HW1-JeremiahV.c
+++ HW1/HW1-JeremiahV.c
@@ -1,8 +1,8 @@
 /*****
  * prob3_starter.c - starter code for nursery rhyme counter
  * 
- * @author: <Your Name> (<Your email)
- * @date:   <Creation date>
+ * @author: <Jeremiah Vandagrift> (j.c.c.vandagrift@gmail.com)
+ * @date:   <10-12-2024>
  * 
  * This program counts things like in the "one little indian" nursery-rhyme. 
  */
@@ -12,9 +12,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+
+#include "HW1-JeremiahV.h"
  
 // contants, typedefs, global variables, etc. go here
-#define MAXTHINGS  10 
+#define MAXTHINGS  10
+#define MINTHINGS 1
  
 char thing[] = "spider";    // the thing we're counting 
  
@@ -25,34 +28,43 @@ char thing[] = "spider";    // the thing we're counting
 int main() {
 	// greet the user and display the working directory for the application
     // this code should be included in all of the applications you submit for ECE 361
-    printf("TODO: ADD YOUR GREETING MESSAGE\n");
     errno = 0;
     char *buf = getcwd(NULL, 0);    // allocates a buffer large enough to hold the path
     
     if (buf == NULL) {
-        perror("getcwd");
-        printf("Could not display the path\n");
+        printf("ERROR (main): Could not display the path\n");
     }
     else {
-        printf("Current working directory: %s\n", buf);
+        printf("\nCurrent working directory: %s\n", buf);
         free(buf);
     }
-    printf("\n");
-	
-	// TODO: REPLACE THIS CODE WITH YOUR IMPLEMENTATION
+
+    printf("\nGreatings traveler. . .\n"
+    "If you're looking to run HW1 you've come to the right place.\n");
+
+    printf("Lets begin!\n");
+
     for (int i = 1; i <= MAXTHINGS; i++) {
 	
 		if (i == MAXTHINGS)
-            printf("%2d little %s bugs\n", i, thing);
+            printf("\n%s little %s bugs\n", getNumStrName(i), thing);
         else if (i % 3 == 0)
-            printf("%2d little %ss\n", i, thing);
+            printf(" %s little %ss\n", getNumStrName(i), thing);
         else
-            printf (" %2d little,", i);
+            printf (" %s little,", getNumStrName(i));
+	}
+
+    printf("\n\n");
+
+    for (int i = 10; i >= MINTHINGS; i--) {
+	
+		if (i == MINTHINGS)
+            printf("\n%s little %s bugs\n", getNumStrName(i), thing);
+        else if (i % 3 == 2)
+            printf(" %s little %ss\n", getNumStrName(i), thing);
+        else
+            printf (" %s little,", getNumStrName(i));
 	}
 	return 0;
 }
- 
- /***** helper functions *****/
- 
- // PUT ADDITIONAL FUNCTIONS HERE
  
