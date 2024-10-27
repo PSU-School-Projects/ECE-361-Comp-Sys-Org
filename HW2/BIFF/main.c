650be9c66989cca543eadb960013d4975f6d428c
jvandag <j.c.vandagrift@gmail.com>
2024-10-27 02:18:58 -0700

    fix: fixed biff text also capitolizing previous char when two or more 9's were placed in a row. Also added continuous input and exiting capabilities

--- HW2/BIFF/main.c
+++ HW2/BIFF/main.c
@@ -15,7 +15,7 @@
 #include "helperFuncs.h"
 #include "../libs/lineHelper.h"
 
-#define MAX_LENGTH 50
+#define MAX_LENGTH 100
 
 int main() {
     // greet the user and display the working directory for the application
@@ -33,13 +33,24 @@ int main() {
 
     printf("\nGreatings traveler. . .\n"
     "If you're looking to run HW2 you've come to the right place.\n"
-    "Lets begin!\n\n");
+    "Lets begin!\n");
     /*********END of Greeting**********/
 
-    char str[MAX_LENGTH];
-    printf("Input BIFF message:\n");
-    getaLine(str, MAX_LENGTH);
-    char trans_str[MAX_LENGTH];
-    BIFF_translate(str, trans_str);
-    printf("BIFF Message: %s\nTranslation: %s\n", str, trans_str);
+    bool exit = false;
+    do {
+        char str[MAX_LENGTH];
+        printf("\nInput BIFF message:\n");
+        getaLine(str, MAX_LENGTH);
+        if (str[0] == '\n' || str[0] == EOF || strlen(str) == 0) {
+            printf("No (more) content received.\n\"Gracefully\" exiting...\n");
+            exit = true; 
+        }
+        else {
+            char trans_str[MAX_LENGTH];
+            BIFF_translate(str, trans_str);
+            printf("BIFF Message: %sTranslation: %s", str, trans_str);
+        }
+    } while (!exit);
+
+    return 0;
 }
