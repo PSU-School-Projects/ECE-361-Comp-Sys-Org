b399d16642f80b7e8ce200c8f046cadf9fd96ee4
jvandag <j.c.vandagrift@gmail.com>
2024-12-10 17:16:27 -0800

    End of term push

---
+++ Final/main.c
@@ -0,0 +1,39 @@
+/**
+ * main.c source file
+ *
+ * @file:		main.c
+ * @author:		Jeremiah Vandagrift (jcv3@pdx.edu)
+ * @date:		12-Dec-2023
+ * @version:	1.0
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include "helperFuncs.h"
+
+int main() {
+
+    return 0;
+}
+
+// char* reverse(char* s) {
+//     int len = strlen(s);
+//     for (int i = 0; i < len; i++) {
+//         if (isFull()) {
+//             printf("Stack could not fit the whole string, reversing incomplete string");
+//             break;
+//         }
+//         if(s != '\0') push(s[i]);
+//     }
+//     while (!isEmpty()) {
+//         int i = 0;
+//         s[i] = pop();
+//     }
+//     return s;
+// }
